1405ef7cdff7d1e0e5bf2857016741e21e1832d4
Marco Filho <marcofilho3520@gmail.com>
2025-05-13 13:28:25 -0300

    Fix: Script menu mobile

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -31,9 +31,5 @@ window.addEventListener('load', function () {
 
 function toggleMenu() {
     const menu = document.getElementById("mobile-menu");
-    if (menu.style.display === "flex") {
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "flex";
-    }
+    menu.classList.toggle("active");
 }
